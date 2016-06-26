16dfc505ed9f975a345f3777e25fce94b6681823
muriel27 <mjw553@nyu.edu>
2016-06-26 02:07:38 -0400

    modify function

--- my_emoji.py
+++ my_emoji.py
@@ -3,9 +3,10 @@
 from enum import Enum
 import os
 import operator
+import random
 
 class Category(Enum):
-	PeopleFace ='face'
+	Face ='face'
 	CatFace ='face'
 	MonkeyFace = 'face'
 	Transport = 'transport'
@@ -13,41 +14,41 @@ class Category(Enum):
 	Animal = 'animal'
 
 class Emoji(Enum):
-	Grin = ('\U0001F601', Category.PeopleFace.name, ['happy','joy','grad'])
-	Tear = ('\U0001F602', Category.PeopleFace.name, ['happy','joy','grad'])
-	Smile = ('\U0001F603', Category.PeopleFace.name, ['happy','joy','grad','smiling'])
-	Mouth = ('\U0001F604', Category.PeopleFace.name, ['happy','joy','grad','smiling'])
-	Sweat = ('\U0001F605', Category.PeopleFace.name, ['happy','joy','grad','smiling'])
-	XD = ('\U0001F606', Category.PeopleFace.name, ['happy','joy','grad','smiling'])
-	Wink = ('\U0001F609', Category.PeopleFace.name, ['happy','joy','grad','smiling'])
-	Savor = ('\U0001F60B', Category.PeopleFace.name, ['happy','joy','grad','smiling'])
-	Relieve = ('\U0001F60C', Category.PeopleFace.name, ['none'])
-	HeartShaped = ('\U0001F60D', Category.PeopleFace.name, ['love','happy','joy','grad','smiling'])
-	Smirk = ('\U0001F60F', Category.PeopleFace.name, ['none'])
-	Unamused = ('\U0001F612', Category.PeopleFace.name, ['none'])
-	ColdSweat = ('\U0001F613', Category.PeopleFace.name, ['none'])
-	Pensive = ('\U0001F614', Category.PeopleFace.name, ['none'])
-	Confound = ('\U0001F616', Category.PeopleFace.name, ['confuse','puzzle'])
-	Kiss = ('\U0001F618', Category.PeopleFace.name, ['none'])
-	Tongue = ('\U0001F61C',Category.PeopleFace.name, ['wink','stuck-out'])
-	ClosedEyes = ('\U0001F61D', Category.PeopleFace.name, ['none'])
-	Disappoint = ('\U0001F61E', Category.PeopleFace.name, ['none'])
-	Angry = ('\U0001F620', Category.PeopleFace.name, ['none'])
-	Pout = ('\U0001F621', Category.PeopleFace.name, ['none'])
-	SadCry = ('\U0001F622', Category.PeopleFace.name, ['none'])
-	Worry = ('\U0001F623', Category.PeopleFace.name, ['worried','persevere','persevering'])
-	Triumph = ('\U0001F624', Category.PeopleFace.name, ['none'])
-	Relieved = ('\U0001F625', Category.PeopleFace.name, ['disappoint'])
-	Fear = ('\U0001F628', Category.PeopleFace.name, ['none'])
-	Weary = ('\U0001F629', Category.PeopleFace.name, ['none'])
-	Sleep = ('\U0001F62A', Category.PeopleFace.name, ['none'])
-	Tired = ('\U0001F62B', Category.PeopleFace.name, ['none'])
-	LoudCry = ('\U0001F62D', Category.PeopleFace.name, ['none'])
-	Scream = ('\U0001F631', Category.PeopleFace.name, ['fear','shock','scare'])
-	Astonished = ('\U0001F632', Category.PeopleFace.name, ['shock'])
-	Flushed = ('\U0001F633', Category.PeopleFace.name, ['blush'])
-	Dizzy = ('\U0001F635', Category.PeopleFace.name, ['none'])
-	MedicalMask = ('\U0001F637', Category.PeopleFace.name, ['ill','sick'])
+	Grin = ('\U0001F601', Category.Face.name, ['happy','joy','grad'])
+	Tear = ('\U0001F602', Category.Face.name, ['happy','joy','grad'])
+	Smile = ('\U0001F603', Category.Face.name, ['happy','joy','grad','smiling'])
+	Mouth = ('\U0001F604', Category.Face.name, ['happy','joy','grad','smiling'])
+	Sweat = ('\U0001F605', Category.Face.name, ['happy','joy','grad','smiling'])
+	XD = ('\U0001F606', Category.Face.name, ['happy','joy','grad','smiling'])
+	Wink = ('\U0001F609', Category.Face.name, ['happy','joy','grad','smiling'])
+	Savor = ('\U0001F60B', Category.Face.name, ['happy','joy','grad','smiling'])
+	Relieve = ('\U0001F60C', Category.Face.name, ['none'])
+	HeartShaped = ('\U0001F60D', Category.Face.name, ['love','happy','joy','grad','smiling'])
+	Smirk = ('\U0001F60F', Category.Face.name, ['none'])
+	Unamused = ('\U0001F612', Category.Face.name, ['none'])
+	ColdSweat = ('\U0001F613', Category.Face.name, ['none'])
+	Pensive = ('\U0001F614', Category.Face.name, ['none'])
+	Confound = ('\U0001F616', Category.Face.name, ['confuse','puzzle'])
+	Kiss = ('\U0001F618', Category.Face.name, ['none'])
+	Tongue = ('\U0001F61C',Category.Face.name, ['wink','stuck-out'])
+	ClosedEyes = ('\U0001F61D', Category.Face.name, ['none'])
+	Disappoint = ('\U0001F61E', Category.Face.name, ['none'])
+	Angry = ('\U0001F620', Category.Face.name, ['none'])
+	Pout = ('\U0001F621', Category.Face.name, ['none'])
+	SadCry = ('\U0001F622', Category.Face.name, ['none'])
+	Worry = ('\U0001F623', Category.Face.name, ['worried','persevere','persevering'])
+	Triumph = ('\U0001F624', Category.Face.name, ['none'])
+	Relieved = ('\U0001F625', Category.Face.name, ['disappoint'])
+	Fear = ('\U0001F628', Category.Face.name, ['none'])
+	Weary = ('\U0001F629', Category.Face.name, ['none'])
+	Sleep = ('\U0001F62A', Category.Face.name, ['none'])
+	Tired = ('\U0001F62B', Category.Face.name, ['none'])
+	LoudCry = ('\U0001F62D', Category.Face.name, ['none'])
+	Scream = ('\U0001F631', Category.Face.name, ['fear','shock','scare'])
+	Astonished = ('\U0001F632', Category.Face.name, ['shock'])
+	Flushed = ('\U0001F633', Category.Face.name, ['blush'])
+	Dizzy = ('\U0001F635', Category.Face.name, ['none'])
+	MedicalMask = ('\U0001F637', Category.Face.name, ['ill','sick'])
 	GrinCat = ('\U0001F638', Category.CatFace.name, ['happy','joy','grad'])
 	TearCat = ('\U0001F639', Category.CatFace.name, ['happy','joy','grad'])
 	SmileCat = ('\U0001F63A', Category.CatFace.name, ['happy','joy','grad','smiling'])
@@ -55,20 +56,17 @@ class Emoji(Enum):
 	WrySmileCat = ('\U0001F63C', Category.CatFace.name, ['none'])
 	KissCat = ('\U0001F63D', Category.CatFace.name, ['none'])
 	PoutCat = ('\U0001F63E', Category.CatFace.name, ['none'])
-	SadCryCat = ('\U0001F63F', Category.CatFace.name, ['none'])
-	
-	NoGoodGesture = ('\U0001F645', Category.PeopleFace.name, ['object','deny'])
-	OkGesture = ('\U0001F646', Category.PeopleFace.name, ['agree','yes'])
-	Bowing = ('\U0001F647', Category.PeopleFace.name, ['kneel'])
+	SadCryCat = ('\U0001F63F', Category.CatFace.name, ['none'])	
+	NoGoodGesture = ('\U0001F645', Category.Face.name, ['object','deny'])
+	OkGesture = ('\U0001F646', Category.Face.name, ['agree','yes'])
+	Bowing = ('\U0001F647', Category.Face.name, ['kneel'])
 	SeeNoEvilMonkey = ('\U0001F648', Category.MonkeyFace.name, ['none'])
 	HearNoEvilMonkey = ('\U0001F649', Category.MonkeyFace.name, ['none'])
 	SpeakNoEvilMonkey = ('\U0001F64A', Category.MonkeyFace.name, ['none'])
-
-	RaiseOneHand = ('\U0001F64B', Category.PeopleFace.name, ['raising'])
-	RaiseBothHand = ('\U0001F64C', Category.PeopleFace.name, ['raising'])
-	Frown = ('\U0001F64D', Category.PeopleFace.name, ['none'])
-	FoldedHands = ('\U0001F64F', Category.PeopleFace.name, ['none'])
-
+	RaiseOneHand = ('\U0001F64B', Category.Face.name, ['raising'])
+	RaiseBothHand = ('\U0001F64C', Category.Face.name, ['raising'])
+	Frown = ('\U0001F64D', Category.Face.name, ['none'])
+	FoldedHands = ('\U0001F64F', Category.Face.name, ['none'])
 	Rocket = ('\U0001F680', Category.Transport.name, ['none'])
 	RailwayCar = ('\U0001F683', Category.Transport.name, ['none'])
 	HighSpeed = ('\U0001F684', Category.Transport.name, ['train'])
@@ -103,10 +101,9 @@ class Emoji(Enum):
 	Toilet = ('\U0001F6BD', Category.Symbol.name, ['bathroom','restroom'])
 	WaterCloset = ('\U0001F6BE', Category.Symbol.name, ['wc'])
 	Bath = ('\U0001F6C0', Category.Symbol.name, ['sign'])
-
 	Rat = ('\U0001F400', Category.Animal.name, ['none'])
 	Mouse = ('\U0001F401', Category.Animal.name, ['none'])
-	OX = ('\U0001F402', Category.Animal.name, ['none'])
+	Ox = ('\U0001F402', Category.Animal.name, ['none'])
 	WaterBuffalo = ('\U0001F403', Category.Animal.name, ['none'])
 	Cow = ('\U0001F404', Category.Animal.name, ['none'])
 	Tiger = ('\U0001F405', Category.Animal.name, ['none'])
@@ -128,13 +125,13 @@ def to_emoji(enter):
 
 	flag = False
 	for emoji in Emoji:
-		#find the corresponding name at Emoji.name
+		#find the corresponding word at Emoji.name
 		if(enter.lower().find(emoji.name.lower()) >= 0 or emoji.name.lower().find(enter.lower()) >= 0):
 			flag = True
 			print((emoji.value)[0])
 			break
 
-		#find the corresponding name at Emoji.value[2] which is extra description
+		#find the corresponding word at Emoji.value[2] which is extra description
 		emotion_tup = emoji.value
 		for description in emotion_tup[2]:
 			if(enter.lower().find(description) >= 0 or description.find(enter.lower()) >=0):
@@ -144,18 +141,30 @@ def to_emoji(enter):
 
 		if flag:
 			break
-		#find the corresponding name at category name and value
-		for category in Category:
-			if (category.name.lower().find(enter) >= 0 or enter.find(category.name.lower()) >= 0 or enter.find(category.value) >= 0 or category.value.find(enter) >= 0):
-				flag =True
-				print((emoji.value)[0])
-				break
-		if flag:
-			break
+	
+	if flag is not True:
+		#random select from the emoji list with same category
+		emoji_list = searchCategory(enter)
+		if(len(emoji_list)>0):
+			flag =True
+			print(random.choice(emoji_list))
 
+	#if no corresponding emoji can be found
 	if flag is not True:
 		print('\U0001F631')
-							
+
+#find the corresponding word at category name
+def searchCategory(enter):
+
+	emoji_list = []
+	for emoji in Emoji:
+		category=(emoji.value)[1]
+		if(category.lower().find(enter.lower()) >=0):
+			emoji_list.append((emoji.value)[0])
+
+	return emoji_list	
+									
+
 while True:
 	enter = input("Please enter here :")
 	if enter=='':
